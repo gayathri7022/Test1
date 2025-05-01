887cee9f04cc5d3381814040a312c22e96fec0ac
gayathri7022 <gayathria7022@gmail.com>
2025-05-01 11:45:16 +0530

    Adding initial files

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from student import DB, register, issue_uniform
 
 def test_cases():
-   
+    DB.clear()
     res1 = register("Gayathri", "gayathri@gmail.com", "12345678")
     assert res1 == "Registration successfull"
 
